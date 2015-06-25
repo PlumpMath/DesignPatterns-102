190a851ff5488638ed2ab697b77604596e4e94a5
Toby Zeldenrust <tobyzeldenrust@home.nl>
2015-06-25 16:22:10 +0200

    Added begin of Command Pattern

---
+++ DesignPatternsV2.0/DrawCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsV2._0
+{
+    class DrawCommand : ICommand
+    {
+        private readonly List<GraphShape> _shapeList;
+        private GraphShape.Shape _shape;
+        private Point _startPoint;
+        private Point _endPoint;
+
+
+
+
+        public DrawCommand(List<GraphShape>shapeList, GraphShape.Shape shape, Point startPoint, Point endPoint)
+        {
+            _shapeList = shapeList;
+            _shape = shape;
+            _startPoint = startPoint;
+            _endPoint = endPoint;
+        }
+
+        public void Do()
+        {
+            _shapeList.Add(new GraphShape(_shape,_startPoint.X,_startPoint.Y,_endPoint.X,_endPoint.Y));
+        }
+
+        public void Undo()
+        {
+            
+        }
+    }
+}
