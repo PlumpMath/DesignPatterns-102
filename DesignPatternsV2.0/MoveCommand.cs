15cf7503c34e5d82e9a9f0f8ecb2e98a0e089120
TobyHome <tobyzeldenrust@home.nl>
2015-06-29 22:05:30 +0200

    Added command undo

---
+++ DesignPatternsV2.0/MoveCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsV2._0
+{
+    class MoveCommand
+    {
+
+        public GraphShape Shape;
+        public Point StartShapePoint;
+        public Point EndShapePoint;
+        public Point StartMoveMousePoint;
+
+        private GraphShape _shape;
+
+        public MoveCommand(GraphShape shape)
+        {
+            
+            _shape = shape;
+
+        }
+
+        public void Do()
+        {
+            
+        }
+
+        public void Undo()
+        {
+            
+        }
+    }
+}
